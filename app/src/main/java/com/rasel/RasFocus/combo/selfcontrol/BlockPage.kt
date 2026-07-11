1d379aca1b48f46da19a0b335055566f35f04150
Rasel Mia <bhmrasel60@gmail.com>
2026-07-11 22:53:06 +0600

    Package rename: com.rasel.RasFocus → com.rasel.pdfviewer in BlockPage.kt

--- app/src/main/java/com/rasel/RasFocus/combo/selfcontrol/BlockPage.kt
+++ app/src/main/java/com/rasel/RasFocus/combo/selfcontrol/BlockPage.kt
@@ -14,7 +14,7 @@
 //   BlockPage.dismiss(service)
 // ============================================================
 
-package com.rasel.RasFocus.combo.selfcontrol
+package com.rasel.pdfviewer.combo.selfcontrol
 
 import android.accessibilityservice.AccessibilityService
 import android.content.Context
@@ -29,7 +29,7 @@ import android.view.Gravity
 import android.view.View
 import android.view.WindowManager
 import android.widget.*
-import com.rasel.RasFocus.DataManager
+import com.rasel.pdfviewer.DataManager
 
 object BlockPage {
 
