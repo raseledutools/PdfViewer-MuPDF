0f50e377648d06b8c8b06f3e1178f383ffa0433b
Rasel Mia <bhmrasel60@gmail.com>
2026-07-11 22:53:43 +0600

    Package rename: com.rasel.RasFocus → com.rasel.pdfviewer in DiaryViewModel.kt

--- app/src/main/java/com/rasel/RasFocus/combo/selfcontrol/study_tools/DiaryViewModel.kt
+++ app/src/main/java/com/rasel/RasFocus/combo/selfcontrol/study_tools/DiaryViewModel.kt
@@ -1,4 +1,4 @@
-package com.rasel.RasFocus.combo.selfcontrol.study_tools
+package com.rasel.pdfviewer.combo.selfcontrol.study_tools
 
 import android.app.AlarmManager
 import android.app.Application
